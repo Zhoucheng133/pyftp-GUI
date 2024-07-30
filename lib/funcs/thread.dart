5c9f6a906ba35f1d740d4a81591bdeaf03b85c1c
周诚 <zhoucheng133@outlook.com>
2024-07-30 18:40:52 +0800

    修复无法执行的问题

--- lib/funcs/thread.dart
+++ lib/funcs/thread.dart
@@ -1,33 +1,37 @@
-// ignore_for_file: unnecessary_brace_in_string_interps, prefer_typing_uninitialized_variables
-
-// import 'dart:convert';
-import 'dart:convert';
 import 'dart:io';
 
-class MainServer{
+class MainServer {
   Process? _process;
-  var pid;
-  var pythonPid;
+  int? pid;
+  int? pythonPid;
 
-  void runCmd(String sharePah, String port, bool enableWrite, bool useLogin, String username, String password) async {
-    var cmd="${Platform.isWindows ? 'python' : 'python3' } -m pyftpdlib -p ${port} -d '${sharePah}' ${enableWrite?'-w':''} ${useLogin?'-u $username -P $password':''}";
+  void runCmd(String sharePath, String port, bool enableWrite, bool useLogin, String username, String password) async {
+    var cmd = Platform.isWindows ? 'python' : 'python3';
+    var args = ['-m', 'pyftpdlib', '-p', port, '-d', sharePath];
+    if (enableWrite) {
+      args.add('-w');
+    }
+    if (useLogin) {
+      args.add('-u');
+      args.add(username);
+      args.add('-P');
+      args.add(password);
+    }
+
+
     try {
-      _process = await Process.start(cmd, [], runInShell: true);
-      pid=_process?.pid;
-      _process?.stderr.transform(utf8.decoder).listen((data) {
-        if(data.contains("pid") && data.contains(">>>") && data.contains("<<<")){
-          var st=data.indexOf('pid=');
-          var end=data.indexOf(' <<<');
-          pythonPid=data.substring(st+4, end);
-        }
-      }, onError: (_){});
-    } catch (_) {}
+      _process = await Process.start(cmd, args, runInShell: false, mode: ProcessStartMode.detached);
+      pid = _process?.pid;
+    } catch (_) {
+    }
   }
 
-  void stopCmd(){
-    if(pid!=null){
-      Process.killPid(pid);
-      Process.killPid(int.parse(pythonPid));
+  void stopCmd() {
+    if (_process != null) {
+      try {
+        _process?.kill();
+      } catch (_) {}
+    } else {
     }
   }
 }
